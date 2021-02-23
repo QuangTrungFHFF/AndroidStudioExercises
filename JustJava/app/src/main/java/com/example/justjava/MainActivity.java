2712131953b4f8cac04d5ec22abc05796a9108ba
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2021-02-23 12:55:35 +0100

    Add Increment/Decrement Button

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -4,25 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvQuantity, tvPrice;
+    TextView tvQuantity, tvPrice,btnIncrement, btnDecrement;
+    int quantity = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        btnIncrement = (Button)findViewById(R.id.increment_button);
+        btnDecrement = (Button)findViewById(R.id.decrement_button);
+
+        btnIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                quantity+=1;
+                display(quantity);
+            }
+        });
+
+        btnDecrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(quantity>0)
+                {
+                    quantity-=1;
+                }
+                else
+                {
+                    quantity =0;
+                }
+                display(quantity);
+            }
+        });
+        }
+
+
+
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2*5);
+        //display(quantity);
+        displayPrice(quantity*5);
     }
 
     /**
