e81c694d3f43bf0089a5d83ae0951f37f15284da
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2021-02-25 21:14:38 +0100

    Update function for increment/decrement button

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 quantity+=1;
                 display(quantity);
+                displayPrice(quantity*5);
             }
         });
 
@@ -42,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
                     quantity =0;
                 }
                 display(quantity);
+                displayPrice(quantity*5);
             }
         });
         }
@@ -55,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         //display(quantity);
         displayPrice(quantity*5);
+        Toast toast = Toast.makeText(getApplicationContext(),"Order success!",Toast.LENGTH_SHORT);
+        toast.show();
     }
 
     /**
